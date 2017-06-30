c1a8d2c58fae4ea2a05a31beebc7014adb1c7c3d
marie-urbano <hippchz@gmail.com>
2017-06-30 09:33:54 +0000

    ressoucerie view

--- application/views/main/ressourcerie.php
+++ application/views/main/ressourcerie.php
@@ -1,6 +1,114 @@
 <h1>Com.Une.TV</h1>
 <!--    data-setup='{}'-->
 
+	<div class="row">
+		<div class="col-md-10">
+			<div  class="row">
+				<div class="mignature" class="col-md-2">
+                    <img src="/img/mini.jpg" alt="video">
+				</div>
+				<div class="col-md-2">
+                    <img src="/img/mini.jpg" alt="video">
+				</div>
+				<div class="col-md-2">
+                    <img src="/img/mini.jpg" alt="video">
+				</div>
+				<div class="col-md-2">
+                    <img src="/img/mini.jpg" alt="video">
+				</div>
+				<div class="col-md-2">
+                    <img src="/img/mini.jpg" alt="video">
+				</div>
+				<div class="col-md-2">
+                    <img src="/img/mini.jpg" alt="video">
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-2">
+				</div>
+				<div class="col-md-2">
+				</div>
+				<div class="col-md-2">
+				</div>
+				<div class="col-md-2">
+				</div>
+				<div class="col-md-2">
+				</div>
+				<div class="col-md-2">
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-2">
+				</div>
+				<div class="col-md-2">
+				</div>
+				<div class="col-md-2">
+				</div>
+				<div class="col-md-2">
+				</div>
+				<div class="col-md-2">
+				</div>
+				<div class="col-md-2">
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-2">
+				</div>
+				<div class="col-md-2">
+				</div>
+				<div class="col-md-2">
+				</div>
+				<div class="col-md-2">
+				</div>
+				<div class="col-md-2">
+				</div>
+				<div class="col-md-2">
+				</div>
+			</div>
+		</div>
+		<div class="col-md-2">
+			<form role="form">
+				<div class="form-group">
+
+					<label for="exampleInputEmail1">
+						Catégorie
+					</label>
+					<input type="email" class="form-control" id="exampleInputEmail1" />
+				</div>
+				<div class="form-group">
+
+					<label for="exampleInputPassword1">
+						Auteur
+					</label>
+					<input type="password" class="form-control" id="exampleInputPassword1" />
+				</div>
+				<div class="form-group">
+
+					<label for="exampleInputFile">
+						
+					</label>
+					<input type="file" id="exampleInputFile" />
+					<p class="help-block">
+						Example block-level help text here.
+					</p>
+				</div>
+				<div class="checkbox">
+
+					<label>
+						<input type="checkbox" /> Check me out
+					</label>
+				</div>
+				<button type="submit" class="btn btn-default">
+					Submit
+				</button>
+			</form>
+		</div>
+	</div>
+
+
+
+
+
     <?php foreach($videos as $video): ?>
 <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 film">
     <a href="<?php echo base_url(); ?>video/<?php echo $video['id_VI']; ?>"><div class="col-xs-12 col-sm-12 col-md-12 col-lg-12"><p>Toto</p>
