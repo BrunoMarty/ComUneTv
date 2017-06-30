9d0975912973e1367cce4b639e1c617d1beb8463
bruno <bruno.marty4@gmail.com>
2017-06-30 02:13:38 +0200

    maj

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,60 +1,49 @@
 <?php
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login extends CI_Controller {
 
-	public function __construct() {
+    public function __construct() {
 
-	       parent::__construct();
-		   $this->load->helper('form','url');
-	       $this->load->helper('url_helper');
-		   $this->load->library('form_validation');
-	   }
-
-		public function index() {
-
-	   $this->form_validation->set_rules('email', 'email', 'trim|required|valid_email', array(
-		   'required' => 'Vous devez saisir un %s.',
-		   'min_length' => 'Le nom doit faire 3 caractères minimum',
-	   ));
-	   $this->form_validation->set_rules('password', 'password', 'trim|required|min_length[5]|alpha_dash|callback_session', array(
-		   'required' => 'Vous devez saisir un %s.',
-		   'min_length' => 'Le mot de passe doit faire 5 caractères minimum',
-		   'alpha_dash' => 'Le mot de passe ne doit contenir que des caractères alpha-numérique, des _ ou des -'
-	   ));
-	   if ($this->form_validation->run()==false)
-		  {
-			  $this->load->view('wrong');
-		  } else {
-			 redirect(base_url('login'));
-		  }
-}
-
-		function session($password){
-			$username = $this->input->post('email');
-			$result = $this->login->login($email,$password);
-			if($result){
-				$sess_array = array();
-				foreach ($result as $row) {
-					$sess_array = $arrayName = array('email' => $row->username, 'password' => $row->fullname);
-					$this->session->set_userdata('logged_in', $sess_array);
-				}
-				return true;
-			} else{
-				$this->form_validation->set_message('Invalid email or password :/'); //'basisdata_cek',
-				return false;
-			}
- }
-
-
-
-
-
-
-
-		public function connexion()
-		{
-
-		}
-
+        parent::__construct();
+        $this->load->model('User_Model');
+        $this->load->helper('form', 'url');
+        $this->load->helper('url_helper');
+        $this->load->library('form_validation');
     }
+
+    public function index() {
+
+        $this->form_validation->set_rules('email', 'email', 'trim|required|valid_email', array(
+            'required' => 'Vous devez saisir un %s.',
+            'min_length' => 'Le nom doit faire 3 caractères minimum',
+        ));
+        $this->form_validation->set_rules('password', 'password', 'trim|required|min_length[5]|alpha_dash|callback_session', array(
+            'required' => 'Vous devez saisir un %s.',
+            'min_length' => 'Le mot de passe doit faire 5 caractères minimum',
+            'alpha_dash' => 'Le mot de passe ne doit contenir que des caractères alpha-numérique, des _ ou des -'
+        ));
+
+        $this->load->view('header');
+        $this->load->view('login');
+        $this->load->view('footer');
+    }
+
+    function session($password) {
+        $username = $this->input->post('email');
+        $result = $this->User_Model->login($email, $password);
+        if ($result) {
+            $sess_array = array();
+            foreach ($result as $row) {
+                $sess_array = $arrayName = array('email' => $row->username, 'password' => $row->fullname);
+                $this->session->set_userdata('logged_in', $sess_array);
+            }
+            return true;
+        } else {
+            $this->form_validation->set_message('Invalid email or password :/'); //'basisdata_cek',
+            return false;
+        }
+    }
+
+}
--- application/views/login.php
+++ application/views/login.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html lang="fr">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Login</title>
 
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-  </head>
-  <body>
-
-<div class="container" style="margin-top:100px;">
 
 	<div class="row">
 		<div class="col-md-4"></div>
